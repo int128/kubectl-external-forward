24af61b3abd5c96cc14d5d35e372914d5aa47719
Hidetake Iwata <int128@gmail.com>
2021-03-05 15:32:33 +0900

    Update kubectl-socat.rb

--- kubectl-socat.rb
+++ kubectl-socat.rb
@@ -4,11 +4,11 @@ class KubectlSocat < Formula
   version "v0.1.0"
 
   on_macos do
-    url "https://github.com/int128/kubectl-socat/releases/download/v0.1.0/kubectl-socat_darwin_amd64.zip"
+    url "https://github.com/int128/kubectl-socat/releases/download/v0.1.0/kubectl-socat_v0.1.0_darwin_amd64.zip"
     sha256 "a0862211648202ba80e3c33078ab1c8c2821dc6419e5fa8c496e83500cbfcd34"
   end
   on_linux do
-    url "https://github.com/int128/kubectl-socat/releases/download/v0.1.0/kubectl-socat_linux_amd64.zip"
+    url "https://github.com/int128/kubectl-socat/releases/download/v0.1.0/kubectl-socat_v0.1.0_linux_amd64.zip"
     sha256 "295b650da9766fc4e7ccc80201223f2366b800c419cc3bfab804a2d6e87f4cc0"
   end
 
